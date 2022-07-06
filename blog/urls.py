a862022963d4595fe5e98cb503224726760013f6
Garik77770 <99966350+Garik77770@users.noreply.github.com>
2022-07-06 22:24:22 +0300

    new1

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from blog import views
 
-urlpatterns = [path('', views.home, name='blog-home'),
-               path('about/', views.about, name='blog-about')]
+
+urlpatterns = [path('redirect', views.home, name='blog-home'),
+               path('about/', views.about, name='blog-about'),
+                ]
