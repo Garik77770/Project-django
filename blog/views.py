838b4d253f95eb7d932817aacd8f7e4ac7475fa7
Garik77770 <99966350+Garik77770@users.noreply.github.com>
2022-07-06 13:21:50 +0300

    new

---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'max',
+        "title": "blog post 1",
+        "content": "first post content",
+        "date_posted": 'Feb 25, 2022',
+    },
+    {
+        "author": "Ian",
+        "title": "blog post 2",
+        "content": "Second post content",
+        "date_posted": "Feb 23, 2022",
+    },
+]
+
+
+def home(request):
+    context = {
+        "posts": posts,
+    }
+    return render(request, 'blog/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'about page'
+    }
+    return render(request, 'blog/about.html', context)
+# Create your views here.
