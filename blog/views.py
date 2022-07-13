318dd731f46749d64608c916d9eae550838a42ad
Garik77770 <99966350+Garik77770@users.noreply.github.com>
2022-07-14 02:46:36 +0300

    next update as delete

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,45 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from blog.models import Post
-
-posts = [
-    {
-        'author': 'max',
-        "title": "blog post 1",
-        "content": "first post content",
-        "date_posted": 'Feb 25, 2022',
-    },
-    {
-        "author": "Ian",
-        "title": "blog post 2",
-        "content": "Second post content",
-        "date_posted": "Feb 23, 2022",
-    },
-]
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
 
 
-def home(request):
-    context = {
-        "posts": Post.objects.all(),
-    }
-    return render(request, 'blog/index.html', context)
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    ordering = '-date_posted'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = "/blog/"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 
 def about(request):
--- users/models.py
+++ users/models.py
@@ -10,7 +10,6 @@ class Profile(models.Model):
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
 
