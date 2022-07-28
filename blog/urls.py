636924126196be5288ffd6f2ebdd0262bc9fefce
Garik77770 <99966350+Garik77770@users.noreply.github.com>
2022-07-28 15:39:40 +0300

    add Paginator

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from blog import views
-from blog.views import (PostDetailView, PostCreateView, PostUpdateView, PostDeleteView)
+from blog.views import (PostDetailView,
+                        PostCreateView,
+                        PostUpdateView,
+                        PostDeleteView,
+                        UserPostListView)
 
 urlpatterns = [path('', views.PostListView.as_view(), name='blog-home'),
                path('about/', views.about, name='blog-about'),
@@ -8,4 +12,5 @@ urlpatterns = [path('', views.PostListView.as_view(), name='blog-home'),
                path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
                path('post/new/', PostCreateView.as_view(), name='post-create'),
                path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+               path('user/<username>/', UserPostListView.as_view(), name="user-posts")
                ]
